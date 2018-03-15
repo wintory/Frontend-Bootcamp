50382499ca83e0523149d8c503e12f51c090891e
Wintory <wintory@users.noreply.github.com>
2018-03-15 10:53:29 +0700

    improve async await

--- day2/hw2-2.js
+++ day2/hw2-2.js
@@ -1,45 +1,60 @@
 
-let fetch = require('node-fetch')
+let fetch = require('node-fetch'),peopleSalary
 
-let kk = fetch('http://127.0.0.1:56775/json1.html').then(response => {
-  return response.json();
-})
-.then(peopleSalary => {
-  
 
-  let sumsalary = null
+//fetch('http://127.0.0.1:49700/day2/json1.html').then(async response => {
 
-  let getlow = peopleSalary=>{
-    return peopleSalary.salary<100000
-  }
+  //response.json().then(peopleSalary=>{
+  //  console.log(peopleSalary);
+  //    let peopleLowSalary = peopleSalary.filter(getlow).map(uppersalary)
+  //  console.log("lowersalary*2 = "+peopleLowSalary);
+  //})
+ // let peopleSalary = await response.json();
+  //let peopleLowSalary = peopleSalary.filter(getlow).map(uppersalary)
+  //console.log("lowersalary*2 = "+peopleLowSalary);
+//}).catch(error => {
+  //console.error('Error:', error);
+//});
+
+//self invoking
+! async function(){
+  console.log("Pass");
   
-  let uppersalary = getlow=>{
-    return getlow.salary*2
-  }
-  
+  //let response = await fetch('http://127.0.0.1:49700/day2/json1.html');
+  //let peopleSalary = await response.json();
+  let peopleSalary = await (await fetch('http://127.0.0.1:49700/day2/json1.html')).json();
   let peopleLowSalary = peopleSalary.filter(getlow).map(uppersalary)
   console.log("lowersalary*2 = "+peopleLowSalary);
+let peopleHighSalary = peopleSalary.filter(gethigh).map(re=>re.salary)
+let getAll = peopleHighSalary.concat(peopleLowSalary)
+let sumSalary = getAll.reduce(getsum)
+console.log("sumSalary = "+sumSalary);
+
+}();
+
+(function(){
+  console.log("Pass also");
   
-  
-  let gethigh = peopleSalary=>{
-    return peopleSalary.salary>=100000
-  }
-  
-  let getsum = (sum,inp)=>{
-    return sum+inp
-  }
-  
-  let peopleHighSalary = peopleSalary.filter(gethigh).map(re=>re.salary)
-  
-  let getAll = peopleHighSalary.concat(peopleLowSalary)
-  
-  let sumSalary = getAll.reduce(getsum)
-  
-  console.log("sumSalary = "+sumSalary);
-  
+})();
+
+let sumsalary = null
+
+let getlow = peopleSalary=>{
+  return peopleSalary.salary<100000
+}
+
+let uppersalary = getlow=>{
+  return getlow.salary*2
+}
 
 
-})
-.catch(error => {
-  console.error('Error:', error);
-});
+let gethigh = peopleSalary=>{
+  return peopleSalary.salary>=100000
+}
+
+let getsum = (sum,inp)=>{
+  return sum+inp
+}
+
+
+
