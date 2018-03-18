427a2040b45357a581087d8d6c673511ac631ea5
Wintory <wintory@users.noreply.github.com>
2018-03-18 15:11:27 +0700

    complete hw3-1 and 2

--- day4/page.js
+++ day4/page.js
@@ -1,3 +1,5 @@
+var img = ['pic1', 'pic2', 'pic3', 'pic4', 'pic5'];
+
 $(function () {
     $('#demo')
         .click(function () {
@@ -8,4 +10,17 @@ $(function () {
     $('#exampleModal').on('hidden.bs.modal', function (e) {
         alert('dialog close')
     })
+
+    $.each(img, function (index, value) {
+        $('#carouselExample .carousel-inner').append(`<div class="carousel-item">
+       <h2>${value}</h2>
+    </div>`);
+
+        if (index === 0) {
+            $('#carouselExample .carousel-inner').addClass('active')
+        }
+
+    })
+
+    $('#carouselExample').carousel();
 })
